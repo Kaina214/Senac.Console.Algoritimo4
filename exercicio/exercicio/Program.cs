b620b11f6b31232f30f41f7e518678a61ff91ac7
KAINABRUMHABEKOST <kainabrum123@gmail.com>
2025-04-01 21:44:02 -0300

    URI1001

---
+++ exercicio/exercicio/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+int X = A + B;
+Console.WriteLine("X="+X);
+
